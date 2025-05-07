66a23260d598459d5f53f5b00d9e0d87d61b9615
Lydiahm <109420511+Lydiahm@users.noreply.github.com>
2025-05-07 11:09:00 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
@@ -5,12 +18,36 @@ provider "aws" {
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   skip_credentials_validation = true
+
   endpoints {
     ec2 = "http://ip10-0-2-6-cvh9tgab9qb14bivkplg-4566.direct.lab-boris.fr"
   }
 }
 
+# Génère une valeur aléatoire pour simuler un nouvel AMI à chaque commit
+resource "random_id" "ami_suffix" {
+  byte_length = 2
+}
+
+# Déclare le nombre d'instances directement dans main.tf
+variable "instance_count" {
+  description = "Nombre d'instances EC2 à créer"
+  type        = number
+  default     = 1
+}
+
+# Crée les instances EC2 avec un AMI fictif dynamique
 resource "aws_instance" "demo" {
-  ami           = "ami-12345678"
+  count         = var.instance_count
+  ami           = "ami-${random_id.ami_suffix.hex}"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance-${count.index + 1}"
+  }
+}
+
+# Affiche les IDs des instances créées
+output "instance_ids" {
+  value = aws_instance.demo[*].id
 }
