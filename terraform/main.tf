d9279e2ecdb1d7ae2b2c566774dadb5703bc4470
Lydiahm <109420511+Lydiahm@users.noreply.github.com>
2025-05-07 11:28:44 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,34 +20,38 @@ provider "aws" {
   skip_credentials_validation = true
 
   endpoints {
-    ec2 = "http://ip10-0-2-6-cvh9tgab9qb14bivkplg-4566.direct.lab-boris.fr"
+    ec2 = "http://ip10-0-18-4-d0dh0a005akh4glkf9f0-4566.direct.lab-boris.fr"
   }
 }
 
-# Génère une valeur aléatoire pour simuler un nouvel AMI à chaque commit
-resource "random_id" "ami_suffix" {
-  byte_length = 2
-}
-
-# Déclare le nombre d'instances directement dans main.tf
 variable "instance_count" {
   description = "Nombre d'instances EC2 à créer"
   type        = number
   default     = 1
 }
 
-# Crée les instances EC2 avec un AMI fictif dynamique
+resource "random_pet" "ami_suffix" {
+  length = 2
+  keepers = {
+    # Change à chaque lancement de Jenkins
+    trigger = timestamp()
+  }
+}
+
 resource "aws_instance" "demo" {
   count         = var.instance_count
-  ami           = "ami-${random_id.ami_suffix.hex}"
+  ami           = "ami-${random_pet.ami_suffix.id}"
   instance_type = "t2.micro"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = {
     Name = "Instance-${count.index + 1}"
   }
 }
 
-# Affiche les IDs des instances créées   #Comm
 output "instance_ids" {
   value = aws_instance.demo[*].id
-} 
+}
